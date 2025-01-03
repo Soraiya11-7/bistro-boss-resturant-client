aa6c7080ef697dd588f5b82674fbe0b0c03abdc9
Soraiya11-7 <soraiyaparvin@gmail.com>
2025-01-03 16:56:38 +0600

    Tesmonial and Featured section added on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from '../components/Home/Banner';
 import Category from '../Category/Category';
 import PopularMenu from '../components/Home/PopularMenu';
+import Featured from '../components/Home/Featured';
+import Testimonials from '../components/Home/Testimonials';
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
            <Banner></Banner>
            <Category></Category>
            <PopularMenu></PopularMenu>
+           <Featured></Featured>
+           <Testimonials></Testimonials>
         </div>
     );
 };
